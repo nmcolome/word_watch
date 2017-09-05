603865c014d81d7398338a32e2d27be73412b387
NataliaColome <nmcolome@gmail.com>
2017-09-05 09:59:35 -0600

    finish functionality for feature2

--- src/wordCount.js
+++ src/wordCount.js
@@ -16,8 +16,22 @@ class wordCount {
     const text = $('.text-field').val()
     const cleanedText = text.replace(/(\r\n|\n|\r)/gm," ").replace(/('re)/g, " are").replace(/('m)/g, " am").replace(/('t)/g, " not").replace(/('ll)/g, " will").replace(/('em)/g, " them").replace(/[&\/\\#,+()$~%.'":*-?<>{}]/g, '');
     const words = cleanedText.split(" ")
-    const downcaseWords = words.map((word) => return word.toLowerCase())
-    debugger
+    const downcaseWords = words.map((word) => {return word.toLowerCase()})
+
+    const result = downcaseWords.reduce(function(counterObject, word) {
+      if(!counterObject[word]) {
+        counterObject[word] = 0
+      }
+      counterObject[word] += 1
+      return counterObject
+    }, {})
+
+    //appending to word-count
+    const resultKeys = Object.keys(result)
+    for (let i = 0; i < resultKeys.length; i++) {
+      $('article.word-count').append(`<span class='${resultKeys[i]}-count'>${resultKeys[i]}</span>`)
+      $(`.${resultKeys[i]}-count`).css('margin', '2px').css('font-size', `${result[resultKeys[i]]}em`)
+    }
   }
 }
 
